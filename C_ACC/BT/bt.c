9804149bd8f8fb16e9bc20604c3606b3197b1277
Dasha <ilasovadaria@gmail.ru>
2023-05-02 11:02:44 +0300

    return to working version

--- C_ACC/BT/bt.c
+++ C_ACC/BT/bt.c
@@ -132,13 +132,13 @@ int main(int argc, char *argv[])
   exact_rhs();
   printf(" after exact_rhs\n");
 
-  //#pragma acc enter data create(rho_i[0:KMAX][0:JMAXP+1][0:IMAXP+1],us[0:KMAX][0:JMAXP+1][0:IMAXP+1],\
+  /*#pragma acc enter data create(rho_i[0:KMAX][0:JMAXP+1][0:IMAXP+1],us[0:KMAX][0:JMAXP+1][0:IMAXP+1],\
                                 vs[0:KMAX][0:JMAXP+1][0:IMAXP+1],ws[0:KMAX][0:JMAXP+1][0:IMAXP+1],\
                                 qs[0:KMAX][0:JMAXP+1][0:IMAXP+1],square[0:KMAX][0:JMAXP+1][0:IMAXP+1],\
                                 rhs[0:KMAX][0:JMAXP+1][0:IMAXP+1][5],\
                                 fjac[0:PROBLEM_SIZE+1][PROBLEM_SIZE+1][0:PROBLEM_SIZE+1][0:5][0:5],njac[0:PROBLEM_SIZE+1][PROBLEM_SIZE+1][0:PROBLEM_SIZE+1][0:5][0:5],\
                                 lhs[0:PROBLEM_SIZE+1][PROBLEM_SIZE+1][0:PROBLEM_SIZE+1][0:3][0:5][0:5]) 
-  
+  */
   
   //---------------------------------------------------------------------
   // do one time step to touch all code, and reinitialize
